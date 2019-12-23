6da1625507b971aab08da87dc550b6b3bf4bf7d4
Danny Eck <eck.danny@gmail.com>
2019-12-23 17:41:39 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const store = require("./store");
+
+const app = express();
+const router = express.Router();
+
+router.get("/people/:id", (req, res) => {
+  const person = store["people"][parseInt(req.params.id, 10) - 1];
+  if (!person) {
+    return res.status(404).json({ err: "Not Found" });
+  }
+  res.status(200).json(person);
+});
+
+app.use("/api", router);
+
+module.exports = app;
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}...`);
+  });
+}
